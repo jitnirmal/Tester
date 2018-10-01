646bd463d1fb371d4bc67b17ebe643ff312f53a6
Nirmal <jitnirmal@gmail.com>
2018-10-01 11:02:30 +0800

    added new concepts

--- multithreading/ThreadPool.h
+++ multithreading/ThreadPool.h
@@ -5,39 +5,41 @@
 #include <iostream>
 #include <vector>
 #include <atomic>
+#include <queue>
+#include <unordered_map>
 
 template<typename Data>
 class thread_safe_queue {
 private:
 	std::queue<Data>            _queue;
 	std::mutex					_mutex;
-	const size_t                _limit;
-
 	std::condition_variable		_signal;
+
 	using queue_lock = std::unique_lock<std::mutex>;
-	std::atomic_bool done;
+	std::atomic_bool _stopped;
 
 public:
-	thread_safe_queue(size_t size_limit = 10)
-		: _limit(size_limit), _done(false)
+	thread_safe_queue()
+		: _stopped(false)
 	{}
 
 	void StopQueue()
 	{
-		_done = true;
+		_stopped = true;
 		_signal.notify_all();
 	}
 
 	bool push(const Data& data)
 	{
-		if (_done)
+		if (_stopped)
 		{
 			return false;
 		}
+
 		queue_lock lock(_mutex);
 		_queue.push(data);
 		if (_queue.size() == 1) {
-			_signal.notify_one();
+			_signal.notify_all();
 		}
 		return true;
 	}
@@ -46,62 +48,21 @@ public:
 	{
 		queue_lock lock(_mutex);
 		while (_queue.size()==0) {
-			if (_done == true)
+			if (_stopped == true)
 			{
 				return false;
 			}
-			_signalItemAdded.wait(lock);
+			_signal.wait(lock);
 		}
 		popped_value = _queue.front();
 		_queue.pop();
-		_signalItemRemoved.notify_one();
 		return true;
 	}
-
-	bool try_push(const Data& data)
+	bool empty()
 	{
 		queue_lock lock(_mutex);
-		if (_queue.size() >= _limit) {
-				return false;
-		}
-		_queue.push(data);
-		lock.unlock();
-
-		_signal.notify_one();
-		return true;
+		return _queue.size() == 0;
 	}
-	bool try_pop(Data &popped_value)
-	{
-		queue_lock lock(_mutex);
-		if (_queue.empty()) {
-			return false;
-		}
-
-		popped_value = _queue.front();
-		_queue.pop();
-		_signalItemRemoved.notify_one();
-		return true;
-	}
-	
-	
-
-	bool empty() const
-	{
-		queue_lock lock(_mutex);
-		return _queue.empty();
-	}
-
-	size_t limit() const
-	{
-		return _limit;
-	}
-
-	size_t size() const
-	{
-		queue_lock lock(queue_mutex);
-		return _queue.size();
-	}
-
 };
 
 
@@ -121,17 +82,21 @@ public:
 		}
 	}
 };
+
+
+
 class thread_pool
 {
-	std::atomic_bool done;
+	std::atomic_bool _running;
 	thread_safe_queue<std::function<void()> > work_queue;
 	std::vector<std::thread> threads;
 	join_threads joiner;
 
 	void worker_thread()
 	{
-		while (!done)
+		while (_running || work_queue.empty()==false)
 		{
+
 			std::function<void()> task;
 			if (work_queue.pop(task))
 			{
@@ -139,13 +104,14 @@ class thread_pool
 			}
 			else
 			{
-				//std::this_thread::yield();
+				std::this_thread::yield();
 			}
 		}
+		std::cout <<"stopping : "<< std::this_thread::get_id() << std::endl;
 	}
 public:
 	thread_pool() :
-		done(false), joiner(threads)
+		_running(true), joiner(threads)
 	{
 		unsigned const thread_count = std::thread::hardware_concurrency();
 		try
@@ -154,28 +120,55 @@ public:
 			{
 				threads.push_back(
 					std::thread(&thread_pool::worker_thread, this));
+			
 			}
 		}
 		catch (...)
 		{
-			done = true;
+			_running = false;
 			throw;
 		}
 	}
 
 	~thread_pool()
 	{
-		done = true;
+		work_queue.StopQueue();
+		_running = false;
 	}
 
-	template<typename FunctionType>
-	void submit(FunctionType f)
+	template<class Task, class ...Args>
+	bool submit(Task&& task, Args&& ... args)
 	{
-		work_queue.push(std::function<void()>(f));
+		if (_running) {
+			return work_queue.push(std::bind(std::forward<Task>(task), std::forward<Args>(args)...));
+		}
+		return false;
 	}
 };
 
+
 void TestThreadPool()
 {
-	
+	std::mutex m;
+	std::unordered_map<std::thread::id, int> counter;
+	auto print = [&](const int i, const double d) {
+		std::unique_lock<std::mutex> g(m);
+		counter[std::this_thread::get_id()]++;
+		//std::cout << std::this_thread::get_id() << " : executing event - "<< i  <<" : " << d<< std::endl;
+	};
+	{
+		thread_pool pool;
+		std::cout << "Pool created" << std::endl;
+		for (int i = 0; i < 100000; ++i)
+		{
+			pool.submit(print, i, 3.2f);
+		}
+	}
+	int total = 0;
+	for (const auto& it : counter)
+	{
+		total += it.second;
+
+	}
+
 }
