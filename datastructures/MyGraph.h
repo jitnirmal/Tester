02cfdb7ad07e4e846880f006135b600d65ba3ba4
Nirmal <jitnirmal@gmail.com>
2020-02-02 17:13:33 +1100

    toposort

--- datastructures/MyGraph.h
+++ datastructures/MyGraph.h
@@ -72,8 +72,50 @@ public:
 		std::cout << std::endl;
 	}
 
+	void topologicalSort()
+	{
+		std::vector<int> in_degree(mNumOfVertices);
+		for (const auto& vt : mAdjList)
+		{
+			for (const auto& item : vt)
+				++in_degree[item];
+		}
+
+		std::queue<int> q;
+		for (int i = 0; i < in_degree.size(); ++i)
+		{
+			if (in_degree[i] == 0)
+				q.push(i);
+		}
+
+		int cnt{ 0 };
+		std::vector<int> topSort;
+		while (!q.empty())
+		{
+			auto item = q.front();
+			q.pop();
+			topSort.push_back(item);
+
+			for (const auto& v : mAdjList[item])
+			{
+				if (--in_degree[v] == 0)
+					q.push(v);
+			}
+			++cnt;
+		}
+
+		if (cnt != mNumOfVertices)
+			std::cout << "Topo sort is not possible" << std::endl;
+
+		std::cout << "toposort : ";
+		for (const auto& item : topSort)
+			std::cout << item << " ";
+		std::cout << std::endl;
+
+	}
+
 private:
-	void DFSRecursive(int v, std::vector<bool>& isVisited)
+	void DFSRecursive(int v, std::vector<bool> & isVisited)
 	{
 		isVisited[v] = true;
 		std::cout << v << " ";
@@ -96,6 +138,7 @@ void testGraph()
 	graph g{ { 5,2 }, {5,0}, { 4,0 }, {4,1}, { 2,3 }, {3,1} };
 	g.BFSIterative(5);
 	g.DFSRecursive(5);
+	g.topologicalSort();
 }
 
 
